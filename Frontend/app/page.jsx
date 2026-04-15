6de6727a31729d9f8cc2a21a905f9a0875e36b70
Aayush <aayushs290107@gmail.com>
2026-04-15 10:52:03 +0530

    Added Landing UI Component

---
+++ Frontend/app/page.jsx
@@ -0,0 +1,10 @@
+// import Map from "./components/Map";
+import Component from "./components/ui/landing-page"; // Import the landing page component
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-blue-950 via-blue-900 to-blue-700">
+      <Component/>
+      
+    </main>
+  );
+}
--- Frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Map from "./components/Map";
-export default function Home() {
-  return (
-    <main>
-      <h1>Map Example</h1>
-      <Map />
-    </main>
-  );
-}
