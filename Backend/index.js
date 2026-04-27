99529c694e5c6f7d0a0bfbff6cb217d2317f85f2
Aayush Singh <aayushs290107@gmail.com>
2026-04-27 21:33:10 +0530

    Enhance CORS settings in index.js
    Updated CORS configuration to specify allowed origin and methods.

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,13 @@ const { simulateController } = require("./controllers/simulate");
 
 const app = express();
 
-app.use(cors());
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://enviro-sim.vercel.app", // allow all 
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 // Health check
@@ -17,5 +23,10 @@ app.post("/simulate", simulateController);
 const PORT = process.env.PORT || 6969;
 
 app.listen(PORT, () => {
+<<<<<<< HEAD
   console.log(`🚀 Backend running on port ${PORT}`); //hey
 });
+=======
+  console.log(`🚀 Backend running on port ${PORT}`);
+});
+>>>>>>> 1830e99 (Enhance CORS settings in index.js)
